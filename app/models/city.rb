eb84ad08284ed2ad9a88487c08d718be9c049333
zilliq <zilliqman@gmail.com>
2016-10-13 16:11:32 +0000

    a suppr

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,23 +1,23 @@
 class City < ActiveRecord::Base
   validates :lat, presence: true
   validates :lon, presence: true
-  
+
   before_validation :geocode
-  
+
   before_validation :forecast
-  
+
   private
-  
+
   def geocode
     places = Nominatim.search.city(self.name).limit(1)
     if places.first
       self.lat=places.first.lat
       self.lon=places.first.lon
     end
-  end
-  
-  def forecast
     forecast = ForecastIO.forecast(self.lat, self.lon, params: { units: 'si' })
+    self.temperature = forecast.currently.temperature
+    self.summary = forecast.currently summary
+    self.windSpeed = forecast.currently.windSpeed
   end
-  
+
 end
