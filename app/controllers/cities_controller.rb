ea10a72637c6dc8615fe877f73483ba5c6e3cf34
zilliq <zilliqman@gmail.com>
2016-10-13 16:39:01 +0000

    cities_controller.rb

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -69,6 +69,6 @@ class CitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
-      params.require(:city).permit(:name, :lat, :lon)
+      params.require(:city).permit(:name, :lat, :lon, :summary, :precipProbability, :temperature, :pressure)
     end
 end
