ba033b386de43ffd53cd3878f10832f0cba6354c
zilliq <zilliqman@gmail.com>
2016-10-20 13:58:02 +0000

    liaison students.json

--- public/angular-seed/app/view1/view1.js
+++ public/angular-seed/app/view1/view1.js
@@ -10,7 +10,28 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 
-  .controller('View1Ctrl', ['$scope', function($scope) {
-  $scope.students = [{name: "Tony Stark", surname: "Iron Man"}]
+  .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+    $scope.students=[];
+
+    $http.get('/students.json').success(function(data){
+      $scope.students= data
+    });
+ // $scope.students = [{name: "Tony Stark", surname: "Iron Man"},{name: "Tony", surname: "Man"}]
 
 }]);
+
+
+
+//controller('BoutiqueCtrl', function($scope, $http){
+  //  $scope.articles=[];
+    //$scope.listeArticles=[];
+
+ //   $http.get('json/articles.json').success(function(data){
+   //   $scope.articles= data
+   // });
+
+   // $scope.selectArticles = function(article){
+
+    //}
+
+//  })
