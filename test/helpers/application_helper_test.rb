5cfce6c3c40705d19ab3692ed3f5c94308987b7f
zilliqman <zilliqman@gmail.com>
2016-11-22 06:56:49 -0500

    les tests passent avec un petit +

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
  class ApplicationHelperTest < ActionView::TestCase
    attr_reader :request
    
+   def current_path?(path)
+     assert_dom_equal %{<li class=""><a href="path">toto</a></li>},
+        nav_link('toto', 'path')
+   end
+   
    test 'inactive link' do
       assert_dom_equal %{<li class=""><a href="/cities">toto</a></li>},
         nav_link('toto', '/cities')
