5b2f51d721c5a54e15dc8167d3e5ffed05fe4c07
zilliqman <zilliqman@gmail.com>
2016-11-29 06:12:53 -0500

    fin LPA2 TD

--- config/initializers/omniauth-github.rb
+++ config/initializers/omniauth-github.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   puts "X#{ENV['GITHUB_KEY']}X"
  puts "X#{ENV['GITHUB_SECRET']}X"
- provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+ provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,user:email"
 end
